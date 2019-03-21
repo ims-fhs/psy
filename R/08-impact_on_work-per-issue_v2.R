9db39d3987dc80b2b1fe41198f68e28a9936900f
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-03-21 17:23:28 +0100

    psy_v1.rmd: erste Datenanalyse, Daten 2018

--- R/08-impact_on_work-per-issue_v2.R
+++ R/08-impact_on_work-per-issue_v2.R
@@ -4,6 +4,14 @@ library(ggplot2)
 
 df_raw <- readxl::read_xlsx(path = "data/All Cases 2018.xlsx", sheet = "ICAS210219")
 df <- df_raw
+df$impact_on_work <- as.factor(df$impact_on_work)
+df$impact_on_work <- factor(df$impact_on_work, levels(df$impact_on_work)[c(2,4,1,5,3)])
+
+ggplot(df %>% group_by(gender, impact_on_work) %>%
+         summarise (n = n()) %>%
+         mutate(percentage = 100 * n / sum(n))
+       ,aes(x=impact_on_work, y = percentage, fill = gender))+geom_bar(stat = "identity", position = "dodge")
+
 
 issues <- sort(c("alcohol", "divorce", "work", "conflict", "violence", "trauma", "tax", "stress", "relationship", "redundancy", "mobbing"), decreasing = FALSE)
 sapply(issues, function(x) df <<- df %>% mutate(!!x := grepl(x, df$issues_services, ignore.case = TRUE)))
