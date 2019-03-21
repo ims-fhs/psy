7b8661e60bcf726a43ee59775390b1086b40c3a5
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-03-21 09:40:34 +0100

    use readxl package for import (instead of the slooooow xlsx package). Comment out randomForest experiments.

--- R/00-psy-import.R
+++ R/00-psy-import.R
@@ -1,9 +1,9 @@
 imsbasics::clc()
 library(tidyverse)
 
-df <- xlsx::read.xlsx(file = "data/All Cases 2018.xlsx", sheetName = "ICAS210219", stringsAsFactors = FALSE)[1:10,]
+df <- readxl::read_xlsx(path = "data/All Cases 2018.xlsx", sheet = "ICAS210219")[1:1000,]
 df1 <- df
-df1$issues_services <- gsub("\\s+|:|/", "", df1$issues_services)
+# df1$issues_services <- gsub("\\s+|:|/", "", df1$issues_services)
 df1 <- df1 %>% 
   mutate(issues_services = map(issues_services, ~strsplit(., ',')[[1]] %>% sort),
          I = row_number()) %>% 
@@ -12,14 +12,16 @@ df1 <- df1 %>%
   mutate_at(-(1:17), ~!is.na(.)) %>% 
   select(-I)
 
-df1$impact_on_work <- as.factor(df1$impact_on_work)
-df1 <- df1[,!sapply(df1,function(x) any(is.na(x)))]
-df1 <- df1 %>% mutate_if(is.character, as.factor)
 
-library(randomForest)
 
-set.seed(131)
-psy.rf <- randomForest(impact_on_work ~ ., data = df1, importance=TRUE, proximity=TRUE, na.action=na.omit)
-
-print(psy.rf)
-View(round(importance(psy.rf), 2))
+# df1$impact_on_work <- as.factor(df1$impact_on_work)
+# df1 <- df1[,!sapply(df1,function(x) any(is.na(x)))]
+# df1 <- df1 %>% mutate_if(is.character, as.factor)
+# 
+# library(randomForest)
+# 
+# set.seed(131)
+# psy.rf <- randomForest(impact_on_work ~ ., data = df1, importance=TRUE, proximity=TRUE, na.action=na.omit)
+# 
+# print(psy.rf)
+# View(round(importance(psy.rf), 2))
