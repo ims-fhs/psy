1e5278cf523ca522448857b5014ace278dd656ca
Adrian <trompetenschmid@gmail.com>
2020-06-09 10:24:55 +0200

    cld wip

--- 28-bbb-cld.R
+++ 28-bbb-cld.R
@@ -1,3 +1,4 @@
+imsbasics::clc()
 # devtools::install_github("ims-fhs/cld", ref = "snf")
 library(cld)
 library(magrittr)
@@ -78,21 +79,58 @@ plots$frame10 <- cld %>%
   describe(type = "text", "Diese erste, kurzfristige Möglichkeit der Handlungsregulation kennen alle Individuen. Sie ist jedoch unterschiedlich stark ausgeprägt bezüglich der Geschwindigkeit und Stärke der Anpassung der `Subjektiven Beanspruchung` an eine Veränderung der `Arbeitszufriedenheit und Selbstwirksamkeit`.") %>%
   plot
 
-cld %>%
+plots$frame11 <- cld %>%
   link(ergebnisse %->% arbeitszufriedenheit %->% subjektiv %->% ergebnisse, erwartungen %->% arbeitszufriedenheit) %>%
   link(subjektiv) %>%
   link(ergebnisse) %>%
   describe(type = "text", "Die Veränderung der `Subjektiven Beanspruchung` über die Handlungsbegründung aufgrund der Bedeutung einer veränderten `Arbeitszufriedenheit und Selbstwirksamkeit` ermöglicht in der Folge eine Veränderung der Bedingungen: Bei einer Erhöhung der `Subjektiven Beanspruchung` steigen in der Folge die `Ergebnisse eigener Tätigkeit`, bei einer Senkung der `Subjektiven Beanspruchung` sinken in der Folge auch die `Ergebnisse eigener Tätigkeit`.") %>%
   plot
 
-cld %>%
+plots$frame12 <- cld %>%
   link(ergebnisse %->% arbeitszufriedenheit %->% subjektiv %->% ergebnisse, erwartungen %->% arbeitszufriedenheit) %>%
   link(ergebnisse %->% arbeitszufriedenheit %->% subjektiv %->% ergebnisse) %>%
   link(arbeitszufriedenheit, subjektiv) %>%
   describe(type = "text", "Dieser erste kurzfristig (innert Tagen oder Wochen) wirkende Feedbackprozess erlaubt es, die `Subjektive Beanspruchung` und die `Arbeitszufriedenheit und Selbstwirksamkeit` in Balance zu halten.") %>%
   plot
 
+plots$frame13 <- cld %>%
+  link(ergebnisse %->% arbeitszufriedenheit, erwartungen %->% arbeitszufriedenheit) %>%
+  link(ergebnisse, arbeitszufriedenheit, erwartungen) %>%
+  link(arbeitszufriedenheit) %>%
+  describe(type = "text", "Wie und wohin kann sich nun die `Arbeitszufriedenheit und Selbstwirksamkeit` entwickeln?") %>%
+  plot
 
+plots$frame14 <- cld %>%
+  link(ergebnisse %->% arbeitszufriedenheit, erwartungen %->% arbeitszufriedenheit) %>%
+  link(ergebnisse, arbeitszufriedenheit, erwartungen) %>%
+  link(arbeitszufriedenheit) %>%
+  describe(type = "text", "Sind die Erwartungen konstant höher als die Ergebnisse, erodiert die Arbeitszufriedenheit und strebt gegen 0") %>%
+  describe(type = "ref_mode", 0/0.5 %)% 10/0.2) %>%
+  plot
+
+plots$frame15 <- cld %>%
+  link(ergebnisse %->% arbeitszufriedenheit, erwartungen %->% arbeitszufriedenheit) %>%
+  link(ergebnisse, arbeitszufriedenheit, erwartungen) %>%
+  link(arbeitszufriedenheit) %>%
+  describe(type = "text", "Sind die Erwartungen konstant tiefer als die Ergebnisse, steigt die Arbeitszufriedenheit und strebt gegen 1.") %>%
+  describe(type = "ref_mode", 0/0.5 %(% 10/0.8) %>%
+  plot
+
+plots$frame16 <- cld %>%
+  link(ergebnisse %->% arbeitszufriedenheit, erwartungen %->% arbeitszufriedenheit) %>%
+  link(ergebnisse, arbeitszufriedenheit, erwartungen) %>%
+  link(arbeitszufriedenheit) %>%
+  describe(type = "text", "Sind die Erwartungen und die Ergebnisse immer gleich gross, bleibt die Arbeitszufriedenheit auf dem Ausgangswert.") %>%
+  describe(type = "ref_mode", 0/0.5 %-% 10/0.5) %>%
+  plot
+
+cld %>%
+  link(ergebnisse %->% arbeitszufriedenheit, erwartungen %->% arbeitszufriedenheit) %>%
+  link(ergebnisse, arbeitszufriedenheit, erwartungen) %>%
+  link(arbeitszufriedenheit) %>%
+  describe(type = "text", "Sind die Erwartungen und die Ergebnisse immer gleich gross, bleibt die Arbeitszufriedenheit auf dem Ausgangswert.") %>%
+  describe(type = "ref_mode", 0/0.5 %-% 10/0.5) %>%
+  plot
 
 
 library(officer)
@@ -107,6 +145,12 @@ graph2doc(plots$frame7, file = "rplot1", append = TRUE)
 graph2doc(plots$frame8, file = "rplot1", append = TRUE)
 graph2doc(plots$frame9, file = "rplot1", append = TRUE)
 graph2doc(plots$frame10, file = "rplot1", append = TRUE)
+graph2doc(plots$frame11, file = "rplot1", append = TRUE)
+graph2doc(plots$frame12, file = "rplot1", append = TRUE)
+graph2doc(plots$frame13, file = "rplot1", append = TRUE)
+graph2doc(plots$frame14, file = "rplot1", append = TRUE)
+graph2doc(plots$frame15, file = "rplot1", append = TRUE)
+graph2doc(plots$frame16, file = "rplot1", append = TRUE)
 # graph2ppt(plots$frame5, file = "rplot1", aspectr = 1.7, scaling = 130, upscale = TRUE, append = TRUE)
 # graph2ppt(plots$frame6, file = "rplot1", aspectr = 1.7, scaling = 130, upscale = TRUE, append = TRUE)
 # graph2ppt(plots$frame7, file = "rplot1", aspectr = 1.7, scaling = 130, upscale = TRUE, append = TRUE)
